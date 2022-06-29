84b48acd143a373a77006301018f479edd699641
Mohammad Arman <arman.ctgbd1394@gmail.com>
2022-06-29 20:57:49 +0600

    Login API For Admin and Customer

---
+++ app/Http/Controllers/API/V1/Frontend/APIController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\API\V1\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class APIController extends Controller
+{
+    public function sendSuccessResponse($data,$message)
+    {
+        $response = new stdClass;
+        $response->status = true;
+        $response->data = $data;
+        $response->message = $message;
+        return response()->json($response);
+    }
+
+    public function sendErrorResponse($errors,$message)
+    {
+        $response = new stdClass;
+        $response->status = false;
+        $response->errors = $errors;
+        $response->message = $message;
+        return response()->json($response);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::prefix('v1')->group(function(){
+    Route::post('backend/login',[\App\Http\Controllers\API\V1\Backend\AuthController::class,'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::post('frontend/login',[\App\Http\Controllers\API\V1\Frontend\AuthController::class,'login']);
+
+    Route::group(['middleware' => 'jwt.verify'],function(){
+        
+        Route::prefix('backend')->group(function(){
+            Route::get('profile',[\App\Http\Controllers\API\V1\Backend\AuthController::class,'profile']);
+        });
+
+        Route::prefix('frontend')->group(function(){
+            Route::get('profile',[\App\Http\Controllers\API\V1\Frontend\AuthController::class,'profile']);
+        });
+    });
 });
+
